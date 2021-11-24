e4bac7a1160276688f57861626c7014cb1b60442
baxa88 <mamasolibaxromjon@gmail.com>
2021-11-24 23:20:58 +0500

    heart qushildi

--- script.js
+++ script.js
@@ -43,6 +43,17 @@ function resetInterval() {
   clearInterval(interval);
   interval = setInterval(run, 3000);
 }
+
+const heart = document.querySelectorAll(".fa-heart");
+heart.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.style.color == "red") {
+      item.style.color = "black";
+    } else {
+      item.style.color = "red";
+    }
+  });
+});
 // ! Header-section
 //Modal-section
 const modal = document.querySelector(".modal-section"),
@@ -65,7 +76,55 @@ setTimeout(function () {
   loader.style.opacity = "0";
   setTimeout(function () {
     loader.style.display = "none";
-  }, 8000);
-}, 8000);
+  }, 7000);
+}, 7000);
 
 //   ! Loader
+// Timer
+
+let deadline = "2022-01-01";
+
+function getTime(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date()),
+    days = Math.floor(total / (1000 * 60 * 60 * 24)),
+    seconds = Math.floor((total / 1000) % 60),
+    minutes = Math.floor((total / 1000 / 60) % 60),
+    hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  return {
+    total: total,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
+}
+function getZero(num) {
+  if (num >= 0 && num < 10) {
+    return "0" + num;
+  } else {
+    return num;
+  }
+}
+function setClock(selector, endtime) {
+  const timer = document.querySelector(selector),
+    days = timer.querySelector("#days"),
+    hours = timer.querySelector("#hours"),
+    minutes = timer.querySelector("#minutes"),
+    seconds = timer.querySelector("#seconds"),
+    timeInterval = setInterval(updateClock, 1000);
+
+  updateClock();
+
+  function updateClock() {
+    const time = getTime(endtime);
+    days.innerHTML = getZero(time.days);
+    hours.innerHTML = getZero(time.hours);
+    minutes.innerHTML = getZero(time.minutes);
+    seconds.innerHTML = getZero(time.seconds);
+    if (time.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  }
+}
+setClock(".timer", deadline);
+// ! Timer
